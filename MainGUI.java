032e9138e37ef54c13106854f1039f63ebb5f6c7
Marc Chesebro <Marc.chesebro@gmail.com>
2018-11-06 08:55:00 -0500

    started gui

---
+++ MainGUI.java
@@ -0,0 +1,6 @@
+import javafx.application.Application;
+import javafx.application.Initailizable;
+
+public class MainGUI extends Initializable {
+
+}
--- nap_client.java
+++ nap_client.java
@@ -1,1 +1,12 @@
+import javafx.application.Application;
 
+class nap_client extends Application {
+
+    public void start(Stage primaryStage) throws Exeption {
+        primaryStage.setTitle("NAP client")
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
