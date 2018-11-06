59334de1307bd0183c571d740b582f187ed6b5cc
Daniel Gritters <gritteda@mail.gvsu.edu>
2018-11-06 09:45:02 -0500

    nap_server complete and worked on nap_thread

--- nap_server.java
+++ nap_server.java
@@ -1,1 +1,30 @@
- 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ftp_server {
+
+    public static void main(String[] args) throws Exception{
+
+        String fromClient;
+        String clientCommand;
+        byte[] data;
+
+
+        ServerSocket welcomeSocket = new ServerSocket(12000);
+        String frstln;
+
+        while (true) {
+            //Wait for request at welcome socket
+            Socket connectionSocket = welcomeSocket.accept();
+
+            //create a new thread for the user
+            nap_thread userConnection = new nap_thread(connectionSocket);
+            Thread user_thread = new Thread(userConnection);
+
+            // start the thread
+            user_thread.start();
+        }
+
+    }
+}
