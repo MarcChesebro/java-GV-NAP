912e93a26429755c6301010566f69aaa9bd730f1
Marc Chesebro <Marc.chesebro@gmail.com>
2018-11-06 09:45:19 -0500

    setup gui

--- MainGUI.java
+++
@@ -1,6 +0,0 @@
-import javafx.application.Application;
-import javafx.application.Initailizable;
-
-public class MainGUI extends Initializable {
-
-}
---
+++ src/MainGUI.java
@@ -0,0 +1,3 @@
+public class MainGUI extends Initializable {
+
+}
