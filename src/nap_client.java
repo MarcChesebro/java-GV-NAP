3c628ee057320d540ce11fc7d3ddbae4fff92883
Daniel Gritters <gritteda@mail.gvsu.edu>
2018-11-19 17:59:38 -0500

    minor changes

--- src/nap_client.java
+++ src/nap_client.java
@@ -15,62 +15,58 @@ public class nap_client extends Application {
     }
 
     public static void main(String[] args) {
+	//Spawn a thread that listens and creates threads with a connection.
 	String sentence;
-	String comands = "retr file.txt || quit";
+	String commands = "retr file.txt || quit";
 
-	BufferedReader inFromUser = new BufferedReader(/*this is where that textfield goes*/);
-	sentence = inFromUser.readLine();
+	sentence = "FIXME"; // insert textfield input
 	StringTokenizer tokens = new StringTokenizer(sentence);
 
-	if(sentence.startsWith("connect"))/*the connect is probable going to by the connect button*/{
-	  String serverName = tokens.nextToken();//pass the connect command
-	  serverName = tokens.nextToken(); //this would be the hostname textfield
-	  controlPort = Integer.parsseInt(tokens.nextToken());
-	  System.out.println("Connected to " serverName);
+	if(sentence.startsWith("retrieve"))/*the connect is probable going to by the connect button*/{
+	   String serverName = tokens.nextToken();//pass the retrieve command
+	   serverName = tokens.nextToken(); //this would be the hostname textfield
+	   controlPort = Integer.parsseInt(tokens.nextToken());
 
-	  Socket ControlSocket = new Socket(serverName, controlPort);
+	   Socket ControlSocket = new Socket(serverName, controlPort);
 
-	  DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
-	  Buffered Reader inFromServer = new BufferedReader(new InputStreamReader(ControlSocket.getInputStream()));
+	   DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
+	   BufferedReader inFromServer = new BufferedReader(new InputStreamReader(ControlSocket.getInputStream()));	  
+	   System.out.println("Connected to " + serverName);
+ 
+	   int dataPort = controlPort + 2;
 
-	  sentence = inFromUser.readLine();//this would be the command textbox
+	   outToServer.writeBytes(dataPort + " " + sentence + " " + '\n');
 
-	  while (true) {
-	      if (sentence.startsWith("retr")){
-		int dataPort = controlPort + 2;
+	   ServerSocket welcomeData = new ServerSocket(dataPort);
+	   Socket dataSocket = welcomeData.accept();
 
-		outToServer.writeBytes(dataPort + " " + sentence + " " + '\n');
+	   StringTokenizer user_tokens = new StringTokenizer(sentence);
+	   user_tokens.nextToken(); //skip retr command
+	   String filename = user_tokens.nextToken();
 
-		ServerSocket welcomeData = new ServerSocket(dataPort);
-		Socket dataSocket = welcomeData.accept();
-
-		StringTokenizer user_tokens = new StringTokenizer(sentence);
-		user_tokens.nextToken(); //skip retr command
-		String filename = user_tokens.nextToken();
-
-		String statusCode = inFromServer.readLine();
-		if(!statusCode.startsWith("200")){
-		  //if it was not 200 return bad status code
-		  System.out.println(statusCode);
-		  welcomeData.close();
-		  dataSocket.close();
-		  sentence = inFromUser.readLine();
-		  continue;
-		}
-		BufferedReader inData = new BufferedReader(new InputStreamReader(dataSocket.getInputStream()));
-		BufferedWriter toFile = new BufferedWriter(new FileWriter(filename));
-		String line = inData.readLine();
-		while(line != null){
-		    toFile.write(line);
-		    toFile.newLine();
-		    line = inData.readLine();
-		}
-		toFile.close();
-		welcomeData.close();
-		dataSocket.close();
-		System.out.println("Successfully downloaded " + filename ); 
-	      }
-	  }
+	   String statusCode = inFromServer.readLine();
+	   if(!statusCode.startsWith("200")){
+	   	  //if it was not 200 return bad status code
+	     System.out.println(statusCode);
+	     welcomeData.close();
+	     dataSocket.close();
+	     sentence = inFromUser.readLine();
+	     continue;
+	   }
+	   BufferedReader inData = new BufferedReader(new InputStreamReader(dataSocket.getInputStream()));
+	   BufferedWriter toFile = new BufferedWriter(new FileWriter(filename));
+	   String line = inData.readLine();
+	   while(line != null){
+	       toFile.write(line);
+	       toFile.newLine();
+	       line = inData.readLine();
+	   }
+	   toFile.close();
+	   welcomeData.close();
+	   dataSocket.close();
+	   System.out.println("Successfully downloaded " + filename ); 
+      }
+  }
+	  /***************************************/
       //launch(args);
-    }
 }
