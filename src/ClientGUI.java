9382fab16d0251ef6717b3020d6be1b81680bd83
glynnja <jacksonglynn@yahoo.com>
2018-11-19 19:04:00 -0500

    Starting Gui

---
+++ src/ClientGUI.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class ClientGUI {
+
+  public static void main (String[] args) {
+
+    JFrame frame = new JFrame("Client GUI");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ClientPanel panel = new ClientPanel();
+    frame.getContentPane().add(panel);
+
+    frame.pack();
+    frame.setResizable(false);
+    frame.setVisible(true);
+  }
+}
