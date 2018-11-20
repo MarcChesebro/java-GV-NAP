621573dc66bd8b3456b985151df4a38d3a101354
Marc Chesebro <Marc.chesebro@gmail.com>
2018-11-19 19:11:38 -0500

    ftp should be mostly done

--- src/nap_client.java
+++ src/nap_client.java
@@ -1,97 +1,46 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.StringTokenizer;
 
-public class nap_client extends Application {
+public class nap_client {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("nap/sample.fxml"));
-        primaryStage.setTitle("NAP client");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    private Socket FtpControlSocket;
+    DataOutputStream FtpoutToServer;
+    BufferedReader FtpinFromServer;
+    int FtpcontrolPort;
 
     public static void main(String[] args) {
         //Spawn a thread that listens and creates threads with a connection.
         String sentence;
         String commands = "retr file.txt || quit";
-//      while (true) {
-//	sentence = "FIXME"; // insert textfield input
-//	StringTokenizer tokens = new StringTokenizer(sentence);
-//
-//	if(sentence.startsWith("retrieve"))/*the connect is probable going to by the connect button*/{
-//	   String serverName = tokens.nextToken();//pass the retrieve command
-//	   serverName = tokens.nextToken(); //this would be the hostname textfield
-//	   controlPort = Integer.parsseInt(tokens.nextToken());
-//
-//	   Socket ControlSocket = new Socket(serverName, controlPort);
-//
-//	   DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
-//	   BufferedReader inFromServer = new BufferedReader(new InputStreamReader(ControlSocket.getInputStream()));
-//	   System.out.println("Connected to " + serverName);
-//
-//	   int dataPort = controlPort + 2;
-//
-//	   outToServer.writeBytes(dataPort + " " + sentence + " " + '\n');
-//
-//	   ServerSocket welcomeData = new ServerSocket(dataPort);
-//	   Socket dataSocket = welcomeData.accept();
-//
-//	   StringTokenizer user_tokens = new StringTokenizer(sentence);
-//	   user_tokens.nextToken(); //skip retr command
-//	   String filename = user_tokens.nextToken();
-//
-//	   String statusCode = inFromServer.readLine();
-//	   if(!statusCode.startsWith("200")){
-//	   	  //if it was not 200 return bad status code
-//	     System.out.println(statusCode);
-//	     welcomeData.close();
-//	     dataSocket.close();
-//	     sentence = inFromUser.readLine();
-//	     continue;
-//	   }
-//	   BufferedReader inData = new BufferedReader(new InputStreamReader(dataSocket.getInputStream()));
-//	   BufferedWriter toFile = new BufferedWriter(new FileWriter(filename));
-//	   String line = inData.readLine();
-//	   while(line != null){
-//	       toFile.write(line);
-//	       toFile.newLine();
-//	       line = inData.readLine();
-//	   }
-//	   toFile.close();
-//	   welcomeData.close();
-//	   dataSocket.close();
-//	   System.out.println("Successfully downloaded " + filename );
-//      	}
-//      }
+    }
+
+    public void write_to_ftp_window(String str){
+        // FIXME print to gui when implemented
     }
 
     public void GoButton(String command) throws IOException{
         String sentence = command; // insert textfield input
         StringTokenizer tokens = new StringTokenizer(sentence);
 
-        if (sentence.startsWith("retrieve"))/*the connect is probable going to by the connect button*/ {
-            String serverName = tokens.nextToken();//pass the retrieve command
-            serverName = tokens.nextToken(); //this would be the hostname textfield
-            int controlPort = Integer.parseInt(tokens.nextToken());
+        if(sentence.startsWith("connect")){
+            tokens.nextToken();
+            String serverName = tokens.nextToken();
+            FtpcontrolPort = Integer.parseInt(tokens.nextToken());
 
-            Socket ControlSocket = new Socket(serverName, controlPort);
+            Socket ControlSocket = new Socket(serverName, FtpcontrolPort);
 
-            DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(ControlSocket.getInputStream()));
-            System.out.println("Connected to " + serverName);
+            FtpoutToServer = new DataOutputStream(FtpControlSocket.getOutputStream());
+            FtpinFromServer = new BufferedReader(new InputStreamReader(FtpControlSocket.getInputStream()));
+            write_to_ftp_window("Connected to " + serverName);
 
-            int dataPort = controlPort + 2;
+        }else if (sentence.startsWith("retrieve"))/*the connect is probable going to by the connect button*/ {
 
-            outToServer.writeBytes(dataPort + " " + sentence + " " + '\n');
+            int dataPort = FtpcontrolPort + 2;
+
+            FtpoutToServer.writeBytes(dataPort + " " + sentence + " " + '\n');
 
             ServerSocket welcomeData = new ServerSocket(dataPort);
             Socket dataSocket = welcomeData.accept();
@@ -100,10 +49,10 @@ public class nap_client extends Application {
             user_tokens.nextToken(); //skip retr command
             String filename = user_tokens.nextToken();
 
-            String statusCode = inFromServer.readLine();
+            String statusCode = FtpinFromServer.readLine();
             if (!statusCode.startsWith("200")) {
                 //if it was not 200 return bad status code
-                System.out.println(statusCode);
+                write_to_ftp_window(statusCode);
                 welcomeData.close();
                 dataSocket.close();
             }
@@ -118,10 +67,11 @@ public class nap_client extends Application {
             toFile.close();
             welcomeData.close();
             dataSocket.close();
-            System.out.println("Successfully downloaded " + filename);
+            write_to_ftp_window("Successfully downloaded " + filename);
+        }else if(sentence.startsWith("quit")){
+            write_to_ftp_window("Exiting.....");
+            FtpControlSocket.close();
+
         }
     }
-
-    /***************************************/
-    //launch(args);
 }
